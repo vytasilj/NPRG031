2675dd84efd9a96bfd6f2ffcd65b565be4e1c58c
Jiří Vytasil <jiri.vytasil@gmail.com>
2016-02-26 15:33:25 +0100

    Inicializace projektu. Vytvoření třídy pro reprezentaci stromu. Prázdné metody pro procházení stromu pomocí BFS a DFS.

---
+++ Lesson02/Search/Search/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson02.Tree;
+
+namespace Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> root = InitializeTree(10);
+
+            Node<int> bfs = FindUsingBFS(root, 5);
+            Node<int> dfs = FindUsingDFS(root, 5);
+        }
+
+
+        private static Node<int> InitializeTree(int maxValue)
+        {
+            Node<int> root = new Node<int>(0);
+            for (int i = 1; i <= maxValue; i++)
+            {
+                root.Add(i);
+            }
+            return root;
+        }
+
+        private static Node<int> FindUsingBFS(Node<int> root, int value)
+        {
+            return null;
+        }
+
+        private static Node<int> FindUsingDFS(Node<int> root, int value)
+        {
+            return null;
+        }
+    }
+}
---
+++ Lesson02/Search/Search/Tree/Node.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson02.Tree
+{
+    public class Node<T>
+    {
+        private readonly T _value;
+
+        private Node<T> _parent;
+        private Node<T> _left;
+        private Node<T> _right;
+        
+
+        public Node(T value)
+        {
+            _value = value;
+        }
+
+
+        public T Value
+        {
+            get { return _value; }
+        }
+
+        public Node<T> Parent
+        {
+            get { return _parent; }
+            private set { _parent = value; }
+        }
+
+        public Node<T> Left
+        {
+            get { return _left; }
+            private set { _left = value; }
+        }
+
+        public Node<T> Right
+        {
+            get { return _right; }
+            private set { _right = value; }
+        }
+
+        public bool IsRoot
+        {
+            get { return _parent == null; }
+        }
+
+        public bool IsLeaf
+        {
+            get { return _left == null && _right == null; }
+        }
+
+
+        public void Add(T item)
+        {
+            Node<T> node = GetItemWithOneChild(this);
+            Node<T> child = new Node<T>(item);
+            if (node.Left == null)
+                node.Left = child;
+            else if (node.Right == null)
+                node.Right = child;
+            child.Parent = node;
+        }
+
+        private static Node<T> GetItemWithOneChild(Node<T> root)
+        {
+            List<Node<T>> items = new List<Node<T>>();
+            items.Add(root);
+
+            while (items.Count > 0)
+            {
+                Node<T> first = items[0];
+                items.RemoveAt(0);
+
+                if (first.Left == null || first.Right == null)
+                    return first;
+
+                items.Add(first.Left);
+                items.Add(first.Right);
+            }
+            return null;
+        }
+    }
+}
