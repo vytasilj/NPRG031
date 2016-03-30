11721616b79c4954037879ce3d33234a8955caf6
unknown <Ji Vytasil@sensorite.kti.hide.ms.mff.cuni.cz>
2016-03-30 12:59:34 +0200

    Páté cvičení. Testy

---
+++ Lesson05/Testing/Testing.Tests/SimpleClassTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Testing.Tests
+{
+    [TestFixture] // Označuje třídu s testy
+    public class SimpleClassTests
+    {
+        [Test] // Metoda je test
+        [ExpectedException(typeof(ArgumentNullException))] // V testu bude vyhozená tato výjimka
+        public void CreateSimpleClass_IdIsNull_ThrowException()
+        {
+            new SimpleClass(null, "password");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CreateSimpleClass_IdIsEmpty_ThrowException()
+        {
+            new SimpleClass(string.Empty, "password");
+        }
+
+        [Test]
+        [TestCase(null)] // Spuštění testu se zadaným parametrem
+        [TestCase("")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CreateSimpleClass_InvalidPassword_ThrowException(string pass)
+        {
+            new SimpleClass("id", pass);
+        }
+
+        [Test]
+        public void CreateSimpleClass_ValidInput_AllIsSet()
+        {
+            const string id = "id";
+            const string pass = "pass";
+            SimpleClass simple = new SimpleClass(id, pass);
+
+            // Kontrola, zda je vše nastavené správně.
+            // Pokud podmínka není splněná, tak test skončí s chybou.
+            Assert.That(simple.Id, Is.EqualTo(id));
+            Assert.That(simple.Password, Is.EqualTo(pass));
+        }
+    }
+}
---
+++ Lesson05/Testing/Testing/SimpleClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Testing
+{
+    public class SimpleClass
+    {
+        private readonly string _id;
+        private readonly string _password;
+
+
+        public SimpleClass(string id, string password)
+        {
+            if (string.IsNullOrEmpty(id))
+                throw new ArgumentNullException("id");
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException("password");
+
+            _id = id;
+            _password = password;
+        }
+
+
+        public string Id
+        {
+            get { return _id; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+        }
+    }
+}
