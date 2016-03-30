48fbb7b30f6a4dc416ef7ad0ea1d464d6f1bff06
unknown <Ji Vytasil@sensorite.kti.hide.ms.mff.cuni.cz>
2016-03-30 15:31:30 +0200

    Šesté cvičení. Kopírování souboru.

---
+++ Lesson06/Files/Files/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Na konci using sekce se reader i writer správně ukončí.
+            using (StreamReader reader = new StreamReader("test"))
+            using (StreamWriter writer = new StreamWriter("output"))
+            {
+                writer.WriteLine(reader.ReadLine());
+            }
+        }
+    }
+}
