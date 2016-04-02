7c037f2a69650907d105187e323e4bbc5c6cb837
GenesisCZE <jiri.vytasil@gmail.com>
2016-04-02 23:12:05 +0200

    Oprava zadání úkolu.

---
+++ Lesson06/Trees/Trees/ValueNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Trees
+{
+    /// <summary>
+    /// Prvek stromu, který obsahuje hodnotu.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ValueNode<T> : ITreeNode<T>
+    {
+        public T Value
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
