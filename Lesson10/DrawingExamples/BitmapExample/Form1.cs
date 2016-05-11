34477b39d125415f3b9db11d6c1d3de72e4e8d78
unknown <Ji Vytasil@sensorite.kti.hide.ms.mff.cuni.cz>
2016-05-11 14:03:24 +0200

    Příklady kreslení pro cvičení

---
+++ Lesson10/DrawingExamples/BitmapExample/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitmapExample
+{
+    public partial class Form1 : Form
+    {
+        private readonly Bitmap _bm;
+        private Color _color;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _bm = new Bitmap("image.png");
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(_bm, 0, 0);
+
+            base.OnPaint(e);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    _color = _bm.GetPixel(e.X, e.Y);
+                    break;
+                case MouseButtons.Right:
+                    for (int i = -10; i < 11; i++)
+                    {
+                        int newX = e.X + i;
+                        if (newX < 0 || newX > _bm.Width)
+                            continue;
+                        for (int j = -10; j < 11; j++)
+                        {
+                            int newY = e.Y + j;
+                            if (newY < 0 || newY > _bm.Height)
+                                continue;
+                            _bm.SetPixel(newX, newY, _color);
+                        }
+                    }
+                    Refresh();
+                    break;
+            }
+        }
+    }
+}
---
+++ Lesson10/DrawingExamples/DrawSimpleObjectsExample/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawSimpleObjectsExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            ReDraw();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void ReDraw()
+        {
+            Graphics graphics = CreateGraphics();
+            graphics.Clear(Color.LightGray);
+
+            DrawLine(graphics);
+            DrawCircle(graphics);
+            DrawRectangle(graphics);
+
+            FillCircle(graphics);
+
+            DrawText(graphics);
+        }
+
+        private void DrawLine(Graphics graphics)
+        {
+            Pen myPen = new Pen(Color.Red, 5);
+            graphics.DrawLine(myPen, 20, 20, 200, 210);
+        }
+
+        private void DrawCircle(Graphics graphics)
+        {
+            Pen pen = new Pen(Color.Green, 10);
+            int width = Width / 4;
+            int height = Height / 4;
+            graphics.DrawEllipse(pen, Width/2 - width/2, Height/2 - height/2, width, height);
+        }
+
+        private void DrawRectangle(Graphics graphics)
+        {
+            graphics.DrawRectangle(Pens.Aqua, Width - 120, Height - 90, 100, 50);
+        }
+
+        private void FillCircle(Graphics graphics)
+        {
+            int width = Width / 8;
+            int height = Height / 8;
+            graphics.FillEllipse(Brushes.Yellow, Width / 2 - width / 2, Height / 2 - height / 2, width, height);
+        }
+
+        private void DrawText(Graphics graphics)
+        {
+            Font font = new Font("Helvetica", 40, FontStyle.Bold | FontStyle.Italic);
+            graphics.DrawString("Hello World!", font, Brushes.DarkBlue, 0, (float) Height/2 - 40);
+            
+            Font arialFont = new Font("Arial", 100);
+            graphics.DrawString("ABCD", arialFont, Brushes.Black, 14, 14);
+            graphics.DrawString("ABCD", arialFont, Brushes.Green, 10, 10);
+        }
+    }
+}
